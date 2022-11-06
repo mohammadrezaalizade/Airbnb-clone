e62679d29eebe236579ecb409806424cba3395c7
Mohammadreza Alizadeh <mohammadrezaalizadeh2002@gmail.com>
2022-11-06 14:43:17 +0330

    ADD- Animation to Section comp

--- components/UI/Section.tsx
+++ components/UI/Section.tsx
@@ -1,23 +1,73 @@
-import React, { PropsWithChildren } from 'react'
+import React, { PropsWithChildren, useEffect, useRef, useState } from "react";
+import { motion, useInView, useScroll } from "framer-motion";
 
-
-interface Props extends PropsWithChildren{
-  title:String;
-  comment?:String;
+interface Props extends PropsWithChildren {
+  title: String;
+  comment?: String;
 }
 
-const Section:React.FC<Props> = ({comment,title,children}) => {
+const Section: React.FC<Props> = ({ comment, title, children }) => {
+  const { scrollY } = useScroll();
+  const [currentScroll, setCurrentScroll] = useState(0);
+  useEffect(() => {
+    return scrollY.onChange((latest) => {
+      setCurrentScroll(latest);
+    });
+  }, []);
   return (
-    <section className='flex flex-col gap-3 p-4 select-none'>
-     <div className='flex flex-col gap-1'>
-        <h2 className='text-secondary_dark_gray-600 text-lg font-medium'>{title}</h2>
-        {comment && <p className='text-secondary_dark_gray-300 text-sm'>{comment}</p>}
-     </div>
-      <div className='flex justify-evenly gap-5 flex-col md:flex-row'>
-      {children}
+    <motion.section
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+        transition: {
+          delay: 0.3,
+          duration: 0.4,
+        },
+      }}
+      className="flex flex-col gap-3 p-4 select-none"
+    >
+      <div className="flex flex-col gap-1">
+        <motion.h2
+          className="text-lg font-medium text-secondary_dark_gray-500"
+          initial={{
+            x: "-80vw",
+          }}
+          whileInView={{
+            x: 0,
+            transition: {
+              duration: 0.2,
+              delay: 0.5,
+              type: "tween",
+            },
+          }}
+        >
+          {title}
+        </motion.h2>
+        {comment && (
+          <motion.p
+            className="text-sm text-secondary_dark_gray-400"
+            initial={{
+              opacity:0.8
+            }}
+            whileInView={{
+              opacity:1,
+              transition:{
+                duration:1,
+                delay:0.8,
+              }
+            }}
+          >
+            {comment}
+          </motion.p>
+        )}
       </div>
-    </section>
-  )
-}
+      <div className="flex justify-evenly gap-5 flex-col md:flex-row">
+        {children}
+      </div>
+    </motion.section>
+  );
+};
 
-export default Section
+export default Section;
